1a6a7ab8aa567f54e3cf60a9ac67e93f506bda3e
Janine Tiefenbruck <jlobue@eng.ucsd.edu>
2026-04-08 06:45:19 -0700

    update html for website

--- resources/lectures/lec05/lec05_imports.py
+++ resources/lectures/lec05/lec05_imports.py
@@ -2,7 +2,7 @@ import numpy as np
 import babypandas as bpd
 
 import matplotlib.pyplot as plt
-plt.style.use('ggplot')
+plt.style.use('seaborn-v0_8-colorblind')
 plt.rcParams['figure.figsize'] = (10, 5)
 
 np.set_printoptions(threshold=20, precision=2, suppress=True, legacy='1.21')
@@ -15,6 +15,6 @@ from IPython.display import display, IFrame, YouTubeVideo
 
 def show_grouping_animation():
     src = "https://docs.google.com/presentation/d/1IBMwlqBtnD-Ml1Zhmqs6pnYV6jcYayr3VGFqN9Dg3xk/embed?start=false&loop=false&delayms=60000&rm=minimal"
-    width = 960
-    height = 509
+    width = 600
+    height = 350
     display(IFrame(src, width, height))
